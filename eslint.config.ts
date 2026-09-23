817fb68f1615053ef066d8b7100aaca87a27ec80
Jonathan Terrell <terrell.jm@gmail.com>
2026-09-23 10:59:34 +0200

    add scripts

--- eslint.config.ts
+++
@@ -1,9 +0,0 @@
-/**
- * ESLint configuration.
- */
-
-/** Dependencies - Framework. */
-import dpuse from '@dpuse/eslint-config-dpuse';
-
-/** Exposures - Configuration. */
-export default [...dpuse];
